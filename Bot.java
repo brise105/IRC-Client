0b80defc9a083275b89b9feee9c721463195dbf6
casta145 <casta145@mail.chapman.edu>
2018-04-26 15:30:40 -0700

    Fixed Fib to work with user input

--- Bot.java
+++ Bot.java
@@ -9,22 +9,28 @@ public class Bot extends PircBot {
     }
     public void onMessage(String channel, String sender,
                           String login, String hostname, String message) {
-        if (message.equalsIgnoreCase("time")) {
+        String MSG = message.toLowerCase();
+        if (MSG.equals("time")) {
             String time = new java.util.Date().toString();
             sendMessage(channel, sender + ": The time is now " + time);
-	} else if (message.equalsIgnoreCase("fibonacci")) {
+	} else if (MSG.substring(0,3).equals("fib")) {
             int n1 = 0;
             int n2 = 1;
             int n3;
             int i;
-            int count = 20;
             String sequence = "";
-            sendMessage(channel, sender + ": The First " +  count + " Fibonacci numbers are: ");
-            for (i = 2; i < count; i++) {
-              n3 = n1 + n2;
-              sequence = sequence + Integer.toString(n3) + " ";
-              n1 = n2;
-              n2 = n3;
+            int count = Integer.parseInt(MSG.substring(3,MSG.length()));
+            if (count >= 48) {
+              String error = "Error: Enter a number less than 48.";
+              sendMessage(channel,sender + ": " + error);
+            } else {
+              sendMessage(channel, sender + ": The First " +  count + " Fibonacci numbers are: ");
+              for (i = 2; i < count; i++) {
+                n3 = n1 + n2;
+                sequence = sequence + Integer.toString(n3) + " ";
+                n1 = n2;
+                n2 = n3;
+              }
             }
             sendMessage(channel, sender + ": " + sequence); 
        // }else if (message.equalsIgnoreCase("message")) {
