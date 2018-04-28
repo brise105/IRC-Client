956f55eedfab9b582bdd779f232e5a7d961afc68
brise105 <brise105@mail.chapman.edu>
2018-04-28 00:06:56 -0700

    updates bot w/ commandline style stuff

--- Bot.java
+++ Bot.java
@@ -12,20 +12,20 @@ public class Bot extends PircBot {
         String MSG = message.toLowerCase();
         if (MSG.substring(0,5).equals("!time")) {
             String time = new java.util.Date().toString();
-            sendMessage(channel, sender + ": The time is now " + time);
+            sendMessage(channel, sender + ": the time is now " + time);
 	}
-        else if (MSG.substring(1,4).equals("fib")) {
+        else if (MSG.substring(0,5).equals("!fib ")) {
             int n1 = 0;
             int n2 = 1;
             int n3;
             int i;
             String sequence = "";
-            int count = Integer.parseInt(MSG.substring(4,MSG.length()));
+            int count = Integer.parseInt(MSG.substring(5,MSG.length()));
             if (count >= 48) {
               String error = "Error: Enter a number less than 48.";
               sendMessage(channel,sender + ": " + error);
             } else {
-              sendMessage(channel, sender + ": The First " +  count + " Fibonacci numbers are: ");
+              sendMessage(channel, sender + ": the first " +  count + " Fibonacci numbers are: ");
               for (i = 2; i < count; i++) {
                 n3 = n1 + n2;
                 sequence = sequence + Integer.toString(n3) + " ";
@@ -35,20 +35,20 @@ public class Bot extends PircBot {
             }
             sendMessage(channel, sender + ": " + sequence); 
          }
-         else if (MSG.substring(1,5).equals("fahr")) {
-	     float temp = Float.parseFloat(MSG.substring(5,MSG.length()));
+         else if (MSG.substring(0,6).equals("!fahr ")) {
+	     float temp = Float.parseFloat(MSG.substring(6,MSG.length()));
              temp = ((temp-32)*5)/9;
-             String cel = "Temperature in Celsius: " + temp;
+             String cel = "temperature in Celsius: " + temp;
              sendMessage(channel, sender + ": " + cel);
          }
-         else if (MSG.substring(1,4).equals("cel")) {
-             float temp = Float.parseFloat(MSG.substring(4,MSG.length()));
+         else if (MSG.substring(0,5).equals("!cel ")) {
+             float temp = Float.parseFloat(MSG.substring(5,MSG.length()));
              temp = 9*(temp/5)+32;
-             String fahr = "Temperature in Fahrenheit: " + temp;
+             String fahr = "temperature in Fahrenheit: " + temp;
              sendMessage(channel, sender + ": " + fahr);
          }
-         else if (MSG.substring(1,6).equals("palin")) {
-             String orig = MSG.substring(6,MSG.length());
+         else if (MSG.substring(0,7).equals("!palin ")) {
+             String orig = MSG.substring(7,MSG.length());
              String rev = "";
              String senOut;
              int len = orig.length();
@@ -56,15 +56,15 @@ public class Bot extends PircBot {
                rev = rev + orig.charAt(i);
              }
              if (orig.equals(rev)) {
-               senOut = "Entered string is a palindrome.";
+               senOut = orig + " is a palindrome.";
                sendMessage(channel, sender + ": " + senOut);
              } else {
-               senOut = "Entered string is not a palindrome.";
+               senOut = orig + " is not a palindrome.";
                sendMessage(channel, sender + ": " + senOut);
              }
         }
-        else if (MSG.substring(1,4).equals("rev")) {
-             String orig = MSG.substring(4,MSG.length());
+        else if (MSG.substring(0,5).equals("!rev ")) {
+             String orig = MSG.substring(5,MSG.length());
              String rev = "";
              int len = orig.length();
              for (int i = len - 1; i >= 0; i--) {
@@ -72,8 +72,8 @@ public class Bot extends PircBot {
              }
              sendMessage(channel, sender + ": " + rev);
         }
-        else if (MSG.substring(1,4).equals("cal")) {
-             String equation = MSG.substring(4,MSG.length());
+        else if (MSG.substring(0,5).equals("!cal ")) {
+             String equation = MSG.substring(5,MSG.length());
              if (equation.contains("+")) {
                String[] parts = equation.split("\\+");
                //how do i make it to iterate through multiple parts to not just have only two numbers               
