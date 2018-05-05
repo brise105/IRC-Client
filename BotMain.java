062b3680f1c79a44d6d21c7661c1e36e6c1250d5
Brandon Briseno <35903600+brise105@users.noreply.github.com>
2018-05-05 12:57:17 -0700

    Updates BotMain.java

--- BotMain.java
+++ BotMain.java
@@ -1,3 +1,9 @@
+//Name: Brandon Briseno & Alfonso Castanos
+//SID: 1932124 & 2283681
+//Class: CPSC 353-02 (Dr. Fahy)
+//Program description: this is the entry point for the bot. there is where we instantiatize our connection info.
+//Version: 1.2
+
 import org.jibble.pircbot.*;
 //import Bot;
 
