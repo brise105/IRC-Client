38aff8831291f719ca93dbb7055a4e86da53a95a
casta145 <casta145@mail.chapman.edu>
2018-05-11 01:37:23 -0700

    Fixed code

--- botclient/Bot.java
+++ botclient/Bot.java
@@ -329,7 +329,7 @@ public class Bot extends PircBot {
           sendMessage(channel, sender + ": " + errormsg);
         }
       } else {
-        System.out.println();
+        System.out.print();
       }
     } catch (StringIndexOutOfBoundsException ex) {
       System.out.println("Error: index out of bounds!");
